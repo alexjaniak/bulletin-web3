fec55ba6c5fa443c03f4c9546e4e35155ccc1e6f
alexjaniak <alexander.janiak.acc@outlook.com>
2024-06-29 23:00:55 -0700

    Firestore init & get

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import AppRouter from './AppRouter';
 
+// Init firesbase/store
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore"; 
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "messages"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+  console.log(doc.data().message);
+});
+
 function App() {
   return (
     <div>
