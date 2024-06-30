aa29f3a68541cb895a5e1d3cb77118f0ac24abd6
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-06-29 23:29:38 -0700

    Partially implement echo creation

--- src/components/Echo.jsx
+++ src/components/Echo.jsx
@@ -10,11 +10,13 @@ Echo.propTypes = {
 function Echo({ x, y, message }) {
     const [showMessage, setShowMessage] = useState(false);
 
-    const handleClick = () => {
+    const handleClick = (e) => {
+        e.stopPropagation();
         setShowMessage(!showMessage);
     };
 
     const handleClose = (e) => {
+        e.stopPropagation();
         if (e.target === e.currentTarget) {
             setShowMessage(false);
         }
@@ -26,7 +28,7 @@ function Echo({ x, y, message }) {
         <div className="relative">
             <button
                 className="absolute w-6 h-6 rounded-full bg-transparent border-none p-0 flex justify-center items-center group"
-                style={{ left, top: `${y}px`}}
+                style={{ left, top: `${y}px` }}
                 onClick={handleClick}
             >
                 <div className="bg-white rounded-full w-2 h-2 transform transition-transform duration-300 ease-in-out group-hover:scale-150"></div>
--- src/pages/Bulletin.jsx
+++ src/pages/Bulletin.jsx
@@ -1,21 +1,42 @@
 import Echo from '../components/Echo';
-
-const echoData = [
-  { x: 0.1, y: 50, message: 'Echo 1' },
-  { x: 0.2, y: 100, message: 'Echo 2' },
-  { x: 0.5, y: 150, message: 'Echo 3' },
-  { x: 0.3, y: 1050, message: 'Bony' },
-];
-
+import AddEchoModal from '../components/AddEchoModal';
+import { useState } from 'react';
 
 function Bulletin() {
+  const [echoData, setEchoData] = useState([
+    { x: 0.1, y: 50, message: 'Echo 1' },
+    { x: 0.2, y: 100, message: 'Echo 2' },
+    { x: 0.5, y: 150, message: 'Echo 3' },
+    { x: 0.3, y: 1050, message: 'Bony' },
+  ])
+
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleBackgroundClick = () => {
+    console.log('Background clicked');
+    setShowAddModal(true);
+  };
+
+  const handleAddEcho = (message) => {
+    const newX = 0.5
+    const newY = 300
+    setEchoData([...echoData, { x: newX, y: newY, message }]);
+  };
+
+
   return (
-    <div>
-      <div className= "h-bulletin-height">
+    <div className="relative h-screen" onClick={handleBackgroundClick}>
+      <div className="h-bulletin-height relative">
         {echoData.map((button, index) => (
           <Echo key={index} x={button.x} y={button.y} message={button.message} />
         ))}
       </div>
+      {showAddModal && (
+        <AddEchoModal
+          onClose={() => setShowAddModal(false)}
+          onAddEcho={handleAddEcho}
+        />
+      )}
     </div>
   );
 }
