72bfcdd7cae3bc09b7582f7308836c5656788f73
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-06-30 21:42:07 -0700

    Add echo focuses

--- src/components/AddEchoModal.jsx
+++ src/components/AddEchoModal.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 function AddEchoModal({ onClose, onAddEcho }) {
     const [message, setMessage] = useState('');
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        if (textareaRef.current) {
+          textareaRef.current.focus();
+        }
+      }, []);
 
     const handlePost = () => {
         if (message.trim()) {
@@ -22,11 +29,11 @@ function AddEchoModal({ onClose, onAddEcho }) {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={handleClose}>
             <div className="bg-white shadow-lg w-1/2 max-w-4xl h-1/4 flex flex-col justify-center items-center">
                 <textarea
+                    ref={textareaRef}
                     placeholder="Echo something to the internet..."
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
-                    className="p-2 w-full h-full bg-white text-black outline-none" // Add hover:bg-gray-200 class
-                    style={{ resize: 'none' }}
+                    className="p-2 w-full h-full bg-white text-black outline-none resize-none" // Add hover:bg-gray-200 class
                 />
                 <button
                     onClick={handlePost}
